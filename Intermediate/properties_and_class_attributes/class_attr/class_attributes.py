b5e2bd4e876e8c018e9db66de2b7cdc4be89217b
FantasztikusRokaUr <erathier@gmail.com>
2019-06-18 13:57:06 +0200

    django commits

---
+++ Intermediate/properties_and_class_attributes/class_attr/class_attributes.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 14, 2019
+
+@author: mrfox
+'''
+
+class ShippingContainer:
+    
+    next_serial = 1337
+    
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        
