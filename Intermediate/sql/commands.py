77aabbd2d7ae485707fc7d266cf3f7f946e7346b
FantasztikusRokaUr <erathier@gmail.com>
2019-07-05 13:44:21 +0200

    sql container con

---
+++ Intermediate/sql/commands.py
@@ -0,0 +1,30 @@
+'''
+Created on Jun 25, 2019
+
+@author: mrfox
+'''
+import mysql.connector
+from mysql.connector import Error
+
+def connect():
+    """Connect to MySQL DB"""
+    try:
+        my_db_con = mysql.connector.connect(host='172.19.0.2',
+                                        database='my_data',
+                                        user='root',
+                                        password='dbrpw')
+        if my_db_con.is_connected():
+            print('Connection established')
+        else:
+            print("Couldn't establish connection.")
+    
+    except Error as e:
+        print(e)
+    
+    finally:
+        if my_db_con.is_connected():
+            my_db_con.close()
+
+
+connect()
+        
